1117b08bcced6e3c47c97ff33c53e805419b391a
Jasper <smitzjasper@gmail.com>
2023-06-02 00:52:41 +0200

    fixed some things

--- problems.js
+++ problems.js
@@ -1,7 +1,14 @@
+// Multiply by 5
+// [5, 3, 9] -> [25, 15, 45]
+
+function times5(x) {
+  return x.map(value => (value * 5))
+}
+
 // farenheit to celcius
 // [23, 140, 212, 41] => [-5, 60, 100, 5]
 // (32°F − 32) × 5/9 = 0°C
 
-function getCelcius(farenheit) {
+function getCelsius(farenheit) {
   return farenheit.map(value => (value -32) * 5/9)
 }
--- tests.js
+++ tests.js
@@ -1,10 +1,16 @@
 const chai = window.chai
 const expect = chai.expect
 
-describe('getCelcius', () => {
-  it('should convert farenheit to celcius for all values in an array', () => {
-    expect(getCelcius([23, 140, 212, 41])).to.deep.equal([-5, 60, 100, 5])
-    expect(getCelcius([-58, -22, -4, 14])).to.deep.equal([-50, -30, -20, -10])
-    expect(getCelcius([104, 122, 158, 176])).to.deep.equal([40, 50, 70, 80])
+describe('times5', () => {
+  it('Multiplies array of integers by 5', () => {
+    expect(times5([5, 3, 9])).to.deep.equal([25, 15, 45])
+    expect(times5([1, 0, 7])).to.deep.equal([5, 0, 35])
+  })
+})
+
+describe('getCelsius', () => {
+  it('Converts farenheit to celsius in an array', () => {
+    expect(getCelsius([23, 140, 212, 41])).to.deep.equal([-5, 60, 100, 5])
+    expect(getCelsius([-58, -22, -4, 14])).to.deep.equal([-50, -30, -20, -10])
   })
 })
